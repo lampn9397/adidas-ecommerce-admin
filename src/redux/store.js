a16a5d294b84b448780c3274090e42dde3ee3c67
Phan Ngoc Lam <lampn9397@gmail.com>
2021-11-11 20:13:42 +0700

    Update react-router-connected

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,14 +3,14 @@ import { createBrowserHistory } from 'history';
 import { createStore, applyMiddleware } from 'redux'
 import { routerMiddleware } from 'connected-react-router';
 
-import rootReducers from './reducers'
+import createRootReducer from './reducers'
 
 export const history = createBrowserHistory();
 
 export const sagaMiddleware = createSagaMiddleware()
 
 export default createStore(
-  rootReducers,
+  createRootReducer(history),
   applyMiddleware(
     routerMiddleware(history),
     sagaMiddleware,
