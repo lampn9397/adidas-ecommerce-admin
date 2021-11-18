63353fb1baa64ce63acfb865d6e2390eca2c74cc
duongrong2012 <67502432+duongrong2012@users.noreply.github.com>
2021-11-18 19:50:40 +0700

    cập nhật chức năng xóa cho trang products

--- src/redux/sagas/products.js
+++ src/redux/sagas/products.js
@@ -24,6 +24,34 @@ function* getProducts() {
   alert(errorMessage);
 }
 
+function* deleteProductAction(action) {
+  let errorMessage = '';
+
+  try {
+    const { id } = action.payload;
+
+    const { data } = yield axiosClient.delete('/product', {
+      data: {
+        ids: [id]
+      }
+    });
+
+    if (data.status === responseStatus.OK) {
+      yield put({ type: ActionTypes.DELETE_PRODUCTS_SUCCESS, payload: action.payload });
+      return;
+    }
+
+    errorMessage = data.errors.jwt_mdlw_error;
+  } catch (error) {
+    errorMessage = error.message;
+  }
+
+  yield put({ type: ActionTypes.DELETE_PRODUCTS_FAILED });
+
+  alert(errorMessage);
+}
+
 export default function* appSaga() {
   yield takeLeading(ActionTypes.GET_PRODUCTS, getProducts);
+  yield takeLeading(ActionTypes.DELETE_PRODUCTS, deleteProductAction);
 }
