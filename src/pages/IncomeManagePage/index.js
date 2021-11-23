0d37b5af047d5d0225b182662a50086986c15980
duongrong2012 <67502432+duongrong2012@users.noreply.github.com>
2021-11-23 22:54:50 +0700

    cập nhật giao diện income page

--- src/pages/IncomeManagePage/index.js
+++ src/pages/IncomeManagePage/index.js
@@ -1,82 +1,64 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Table } from 'antd';
-import { Line } from '@ant-design/charts';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Card, Table } from "antd";
+import { Line } from "@ant-design/charts";
 
-import styles from './styles.module.css';
-import * as ActionTypes from '../../redux/actionTypes';
+import styles from "./styles.module.css";
+import * as ActionTypes from "../../redux/actionTypes";
+import { formatCurrency } from "../../utils";
 
 const InComeManagePage = () => {
   const dispatch = useDispatch();
 
-  // const loading = useSelector((state) => state.budget.loading);
-
   const budget = useSelector((state) => state.budget.budgetList);
 
-  const yearbudget = useSelector((state) => state.budget.yearbudgetList);
+  const yearbudget = useSelector((state) => state.budget.yearBudgetList);
 
   const columns2 = [
-    // {
-    //   title: 'Transaction_Id',
-    //   dataIndex: 'Transaction_Id',
-    //   key: 'Transaction_Id',
-    // },
     {
-      title: 'Năm',
-      dataIndex: 'year',
-      key: 'year',
+      title: "Năm",
+      dataIndex: "year",
+      key: "year",
     },
     {
-      title: 'Doanh thu',
-      dataIndex: 'price',
-      key: 'price',
+      title: "Doanh thu",
+      dataIndex: "price",
+      key: "price",
+      render: (text) => formatCurrency(`${text} VNĐ`),
     },
-
   ];
 
   const columns = [
     {
-      title: 'No.',
-      dataIndex: 'product_id',
+      title: "No.",
+      dataIndex: "product_id",
       render: (_, item, index) => index + 1,
     },
     {
-      title: 'Tên sản phẩm',
-      dataIndex: 'name',
+      title: "Tên sản phẩm",
+      dataIndex: "name",
     },
     {
-      title: 'Doanh số',
-      dataIndex: 'quantity',
+      title: "Doanh số",
+      dataIndex: "quantity",
     },
     {
-      title: 'Doanh thu',
-      dataIndex: 'price',
-      render: (price) => `${price} VNĐ`
+      title: "Doanh thu",
+      dataIndex: "price",
+      render: (text) => formatCurrency(`${text} VNĐ`),
     },
   ];
 
-  // const data = [
-  //   { Năm: '1991', value: 3 },
-  //   { Năm: '1992', value: 4 },
-  //   { Năm: '1993', value: 3.5 },
-  //   { Năm: '1994', value: 5 },
-  //   { Năm: '1995', value: 4.9 },
-  //   { Năm: '1996', value: 6 },
-  //   { Năm: '1997', value: 7 },
-  //   { Năm: '1998', value: 9 },
-  //   { Năm: '1999', value: 13 },
-  // ];
-
-  const data = yearbudget;
-
   const config = {
-    data,
-    xField: 'year',
-    yField: 'price',
+    data: yearbudget,
+    xField: "year",
+    yField: "price",
     point: {
       size: 5,
-      shape: 'diamond',
+      shape: "diamond",
     },
+    xAxis: { title: { text: "Năm" } },
+    yAxis: { title: { text: "Doanh thu" } },
   };
 
   React.useEffect(() => {
@@ -97,22 +79,37 @@ const InComeManagePage = () => {
         />
       </div> */}
 
-      <Table
-        rowKey="product_id"
-        columns={columns}
-        dataSource={budget}
-        pagination={{ pageSize: 4 }}
-      />
-
-      { <Table
-        rowKey="Id"
-        columns={columns2}
-        dataSource={yearbudget}
-        pagination={{ pageSize: 4 }}
-      /> }
-      <Line {...config} />
+      <Card
+        title="Danh mục các sản phẩm bán chạy"
+        className={(styles.customerDetailCard, styles.cardSeparator)}
+      >
+        <Table
+          rowKey="product_id"
+          columns={columns}
+          dataSource={budget}
+          pagination={{ pageSize: 4 }}
+        />
+      </Card>
+      {
+        <Card
+          title="Bảng doanh thu theo năm"
+          className={(styles.customerDetailCard, styles.cardSeparator)}
+        >
+          <Table
+            rowKey="Id"
+            columns={columns2}
+            dataSource={yearbudget}
+            pagination={{ pageSize: 4 }}
+          />
+        </Card>
+      }
+      <Card
+        title="Biểu đồ thu nhập theo năm"
+        className={styles.customerDetailCard}
+      >
+        <Line {...config} />
+      </Card>
     </div>
-
   );
-}
+};
 export default InComeManagePage;
