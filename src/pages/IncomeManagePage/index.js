988585ad9c29bcbd976b832e73f0c55bd85304f7
duongrong2012 <67502432+duongrong2012@users.noreply.github.com>
2021-11-25 20:36:22 +0700

    cập nhật biểu đồ

--- src/pages/IncomeManagePage/index.js
+++ src/pages/IncomeManagePage/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Card, Table } from "antd";
+import { Card, Table, Button, DatePicker } from "antd";
 import { Line } from "@ant-design/charts";
 
 import styles from "./styles.module.css";
@@ -13,6 +13,44 @@ const InComeManagePage = () => {
   const budget = useSelector((state) => state.budget.budgetList);
 
   const yearbudget = useSelector((state) => state.budget.yearBudgetList);
+
+  const [state, setState] = React.useState({
+    viewType: 'month',
+    yearSelected: new Date().getFullYear()
+  });
+
+  const selectYear = React.useCallback((date, dateString) => {
+    console.log("sdwd ", date);
+    console.log("sdwd ", dateString);
+    setState((prevState) => ({
+      ...prevState,
+      yearSelected: dateString
+    }))
+    dispatch({
+      type: ActionTypes.GET_YEARBUDGET,
+      payload: {
+        viewType: state.viewType,
+        dateString
+      }
+    });
+    // console.log("sdwd ", pay);
+  }, [dispatch, state.viewType]);
+
+  const onClickChange = React.useCallback((viewType) => () => {
+    console.log("sdwd ", viewType);
+    setState((prevState) => ({
+      ...prevState,
+      viewType
+    }))
+    dispatch({
+      type: ActionTypes.GET_YEARBUDGET,
+      payload: {
+        viewType,
+        dateString: state.yearSelected
+      }
+    });
+    // console.log("sdwd ", pay);
+  }, [dispatch, state.yearSelected]);
 
   const columns2 = [
     {
@@ -51,22 +89,26 @@ const InComeManagePage = () => {
 
   const config = {
     data: yearbudget,
-    xField: "year",
+    xField: state.viewType,
     yField: "price",
     point: {
       size: 5,
       shape: "diamond",
     },
-    xAxis: { title: { text: "Năm" } },
+    xAxis: { title: { text: state.viewType === 'month' ? "Tháng" : 'Năm' } },
     yAxis: { title: { text: "Doanh thu" } },
   };
 
   React.useEffect(() => {
+    const now = new Date()
     dispatch({ type: ActionTypes.GET_BUDGET });
-  }, [dispatch]);
-
-  React.useEffect(() => {
-    dispatch({ type: ActionTypes.GET_YEARBUDGET });
+    dispatch({
+      type: ActionTypes.GET_YEARBUDGET,
+      payload: {
+        dateString: now.getFullYear(),
+        viewType: 'month'
+      }
+    });
   }, [dispatch]);
 
   return (
@@ -104,8 +146,27 @@ const InComeManagePage = () => {
         </Card>
       }
       <Card
-        title="Biểu đồ thu nhập theo năm"
+        title="Biểu đồ doanh thu theo năm"
         className={styles.customerDetailCard}
+        extra={
+          <div>
+            <DatePicker onChange={selectYear} picker="year" />
+            <Button
+              type="primary"
+              className={styles.buttonSeparator}
+              onClick={onClickChange("month")}
+            >
+              Tháng
+            </Button>
+            <Button
+              type="primary"
+              className={styles.buttonSeparator}
+              onClick={onClickChange("year")}
+            >
+              Năm
+            </Button>
+          </div>
+        }
       >
         <Line {...config} />
       </Card>
--- src/redux/sagas/budget.js
+++ src/redux/sagas/budget.js
@@ -4,48 +4,51 @@ import * as ActionTypes from '../actionTypes';
 import { axiosClient, responseStatus } from '../../constants';
 
 function* getBudgetAction() {
-    let errorMessage = '';
-  
-    try {
-      const { data } = yield axiosClient.get('/order/admin/budget-product');
-  
-      if (data.status === responseStatus.OK) {
-        yield put({ type: ActionTypes.GET_BUDGET_SUCCESS, payload: data.results });
-        return;
-      }
-  
-      errorMessage = data.errors.jwt_mdlw_error;
-    } catch (error) {
-      errorMessage = error.response?.data?.errors?.jwt_mdlw_error ?? error.message;
+  let errorMessage = '';
+
+  try {
+    const { data } = yield axiosClient.get('/order/admin/budget-product');
+
+    if (data.status === responseStatus.OK) {
+      yield put({ type: ActionTypes.GET_BUDGET_SUCCESS, payload: data.results });
+      return;
     }
-  
-    yield put({ type: ActionTypes.GET_BUDGET_FAILED });
-  
-    alert(errorMessage);
+
+    errorMessage = data.errors.jwt_mdlw_error;
+  } catch (error) {
+    errorMessage = error.response?.data?.errors?.jwt_mdlw_error ?? error.message;
   }
 
-  function* getYearBudgetAction() {
-    let errorMessage = '';
-  
-    try {
-      const { data } = yield axiosClient.get('/order/admin/budget-date?date=19-12-2021&group=year');
-  
-      if (data.status === responseStatus.OK) {
-        yield put({ type: ActionTypes.GET_YEARBUDGET_SUCCESS, payload: data.results });
-        return;
-      }
-  
-      errorMessage = data.errors.jwt_mdlw_error;
-    } catch (error) {
-      errorMessage = error.response?.data?.errors?.jwt_mdlw_error ?? error.message;
+  yield put({ type: ActionTypes.GET_BUDGET_FAILED });
+
+  alert(errorMessage);
+}
+
+function* getYearBudgetAction(action) {
+  let errorMessage = '';
+
+  try {
+    const { payload } = action;
+    console.log(payload)
+
+    const { data } = yield axiosClient.get(`/order/admin/budget-date?date=01-01-${payload.dateString}&group=${payload.viewType}`);
+
+    if (data.status === responseStatus.OK) {
+      yield put({ type: ActionTypes.GET_YEARBUDGET_SUCCESS, payload: data.results });
+      return;
     }
-  
-    yield put({ type: ActionTypes.GET_YEARBUDGET_FAILED });
-  
-    alert(errorMessage);
+
+    errorMessage = data.errors.jwt_mdlw_error;
+  } catch (error) {
+    errorMessage = error.response?.data?.errors?.jwt_mdlw_error ?? error.message;
   }
 
-  export default function* appSaga() {
-    yield takeLeading(ActionTypes.GET_BUDGET, getBudgetAction);
-    yield takeLeading(ActionTypes.GET_YEARBUDGET, getYearBudgetAction);
-  }
+  yield put({ type: ActionTypes.GET_YEARBUDGET_FAILED });
+
+  alert(errorMessage);
+}
+
+export default function* appSaga() {
+  yield takeLeading(ActionTypes.GET_BUDGET, getBudgetAction);
+  yield takeLeading(ActionTypes.GET_YEARBUDGET, getYearBudgetAction);
+}
